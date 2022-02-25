7c581befade6f85f456593f1782e8d6bd26a3294
ArthurBeaulieu <contact@abeaulieu.fr>
2022-02-25 09:36:57 +0100

    add live...! release

--- src/forge.js
+++ src/forge.js
@@ -1,7 +1,7 @@
 import './forge.scss';
 
 
-const RELEASES = 6;
+const RELEASES = 7;
 
 
 class Forge {
@@ -62,32 +62,35 @@ class Forge {
 
 		const composer = { fr: 'Compositeur', en: 'Composer' };
 		const author = { fr: 'Auteur', en: 'Author' };
-		const tracks = ['Rock', 'Etica', 'Forge', 'Pox', 'Dur', 'Macadam'];
-		const band = ['The Forge', 'The Forge', 'The Forge', 'Small Pox', 'Le Forge', 'Macadam Forge'];
-		const times = ['4:07', '32:44', '39:00', '29:35', '19:00', '8:11'];
+		const allBy = { fr: 'Toutes les pistes par', en: 'All tracks by' };
+		const except = { fr: 'Sauf {{track}} par', en: 'Except {{track}} by' };
+		const tracks = ['Rock', 'Etica', 'Forge', 'Live', 'Pox', 'Dur', 'Macadam'];
+		const band = ['The Forge', 'The Forge', 'The Forge', 'Small Pox', 'Small Pox', 'Le Forge', 'Macadam Forge'];
+		const times = ['4:07', '32:44', '39:00', '1:17:33', '29:35', '19:00', '8:11'];
 		const tracklist = [
 			`<h3>1. Rock'n'Roll – 4:07</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne</p>`,
 			`<h3>1. R U S H – 5:38</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Paul Boulogne<br>Tom Souty, Vincent Boulogne</p><h3>2. Metalfloyd – 6:41</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Paul Boulogne<br>Tom Souty, Vincent Boulogne</p><h3>3. Acoustica – 6:59</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Paul Boulogne<br>Tom Souty, Vincent Boulogne</p><h3>4. Etica Nikomak – 13:26</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Paul Boulogne<br>Tom Souty, Vincent Boulogne</p>`,
 			`<h3>1. Can You Heal My Pain? – 5:16</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>2. Doliprane – 6:13</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>3. Far From City – 4:46</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>4. Fight Is Always Run – 7:57</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>5. Mirage – 6:35</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>6. Heart Of Stone – 8:10</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p>`,
+			`<h3>${allBy[this._lang]}</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>${except[this._lang].replace('{{track}}', "I Don't Need No Doctor")}</h3><p><i>${composer[this._lang]}</i> : Ashford & Simpson, Jo Armstead<br><i>${author[this._lang]}</i> : John Mayer</p><h3>1. Raoul – 4:15</h3><h3>2. Sweet Little Way – 6:10</h3><h3>3. We Want To Know – 4:56</h3><h3>4. Close My Eyes – 8:58</h3><h3>5. Can You Heal My Pain? – 5:00</h3><h3>6. Doliprane – 6:12</h3><h3>7. Fight Is Always Run – 7:15</h3><h3>8. Heart Of Stone – 7:40</h3><h3>9. I Don't Need No Doctor – 6:22</h3><h3>10. Rock'n'Roll – 3:57</h3><h3>11. Just The Reality – 6:17</h3><h3>12. Instru – 4:25</h3><h3>13. Mirage – 6:00</h3>`,
 			`<h3>1. Just The Reality – 7:14</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>2. Doliprane – 6:42</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>3. Mirage – 5:59</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p><h3>4. Close My Eyes – 9:37</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne<br><i>${author[this._lang]}</i> : Stéphane Philippe</p>`,
 			`<h3>1. Président Samouraï – 5:14</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Michel Mathieu, Vincent Boulogne<br><i>${author[this._lang]}</i> : Michel Mathieu</p><h3>2. In / Out – 5:44</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Michel Mathieu, Vincent Boulogne<br><i>${author[this._lang]}</i> : Michel Mathieu</p><h3>3. Aime Moi – 4:47</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Michel Mathieu, Vincent Boulogne<br><i>${author[this._lang]}</i> : Michel Mathieu</p><h3>4. Macadam – 3:13</h3><p><i>${composer[this._lang]}</i> : Luc Boulogne, Michel Mathieu, Vincent Boulogne<br><i>${author[this._lang]}</i> : Michel Mathieu</p>`,
 			`<h3>1. Loulou – 4:03</h3><p><i>${composer[this._lang]}</i> : Christophe Boulogne, Luc Boulogne<br><i>${author[this._lang]}</i> : Michel Mathieu</p><h3>2. Montpar' Blues – 4:08</h3><p><i>${composer[this._lang]}</i> : Christophe Boulogne, Luc Boulogne<br><i>${author[this._lang]}</i> : Michel Mathieu</p>`
 		];
-		const titles = ['Rock\'n\'Roll', 'Etica Nikomak', 'The Forge', 'Small Pox', 'Dur d\'Être Apache...', 'Loulou/Montpar\' Blues'];
-		const spotify = ['', '08D3o3S0y7Y3I2z2YD95Wb', '6JOsCQ7bUWptYKCtA04Jt7', '6v6HlX4bDfFDWrrTRXeQj7', '22mM01wqd6mZNqt41Bqyq4', ''];
-		const apple = ['', 'etica-nikomak/1590605239', 'the-forge/1590930991', 'small-pox-ep/1591523883', 'président-samouraï/1591523511?i=1591523512', ''];
-		const amazon = ['', 'B09JLK7CQJ', 'B09JS8XW45', 'B09K3S3Y66', 'B09K34PWZX', ''];
+		const titles = ['Rock\'n\'Roll', 'Etica Nikomak', 'The Forge', 'Live...!', 'Small Pox', 'Dur d\'Être Apache...', 'Loulou/Montpar\' Blues'];
+		const spotify = ['', '08D3o3S0y7Y3I2z2YD95Wb', '6JOsCQ7bUWptYKCtA04Jt7', '', '6v6HlX4bDfFDWrrTRXeQj7', '22mM01wqd6mZNqt41Bqyq4', ''];
+		const apple = ['', 'etica-nikomak/1590605239', 'the-forge/1590930991', '', 'small-pox-ep/1591523883', 'président-samouraï/1591523511?i=1591523512', ''];
+		const amazon = ['', 'B09JLK7CQJ', 'B09JS8XW45', '', 'B09K3S3Y66', 'B09K34PWZX', ''];
 		//const pandora = ['', '', ''];
-		const tidal = ['', '201425661', '201669802', '202078894', '202078865', ''];
-		const deezer = ['', '266135292', '266746652', '267582302', '267582372', ''];
-		const youtube = ['v=wap0ZVifTfA', 'v=v8yQdRsJah4&list=PLGrV25VkQxOeALepofxUafUZR2_4Hhy8K', 'v=4JecXQAe7nQ&list=PLGrV25VkQxOdBaIl_kHM0Z9mjq_1D3LrK', 'v=VRg5BQkyIiE&list=PLGrV25VkQxOde4ci3lta5oX6WJbPb7iXU', 'v=ZuLNodgTtzI&list=PLGrV25VkQxOdkK4Hjc80bQ5pdcBfB9N_M', 'v=NjlcpdkZnb8&list=PLGrV25VkQxOfFnRPT0QqYXeIk-hNdPvQO'];
-		const discogs = ['21011362-The-Forge-RocknRoll', '20503012-The-Forge-Etica-Nikomak', '20560249-The-Forge-The-Forge', '20548198-Small-Pox-Small-Pox', '20515321-Le-Forge-Dur-dÊtre-Apache', '21012571-Macadam-Forge-Loulou-Montpar-Blues'];
+		const tidal = ['', '201425661', '201669802', '', '202078894', '202078865', ''];
+		const deezer = ['', '266135292', '266746652', '', '267582302', '267582372', ''];
+		const youtube = ['v=wap0ZVifTfA', 'v=v8yQdRsJah4&list=PLGrV25VkQxOeALepofxUafUZR2_4Hhy8K', 'v=4JecXQAe7nQ&list=PLGrV25VkQxOdBaIl_kHM0Z9mjq_1D3LrK', 'https://www.youtube.com/watch?v=xGPS2QmyScc&list=PLGrV25VkQxOdVkj-qUemG65XSXwM5luLu', 'v=VRg5BQkyIiE&list=PLGrV25VkQxOde4ci3lta5oX6WJbPb7iXU', 'v=ZuLNodgTtzI&list=PLGrV25VkQxOdkK4Hjc80bQ5pdcBfB9N_M', 'v=NjlcpdkZnb8&list=PLGrV25VkQxOfFnRPT0QqYXeIk-hNdPvQO'];
+		const discogs = ['21011362-The-Forge-RocknRoll', '20503012-The-Forge-Etica-Nikomak', '20560249-The-Forge-The-Forge', '21077248-Small-Pox-Live', '20548198-Small-Pox-Small-Pox', '20515321-Le-Forge-Dur-dÊtre-Apache', '21012571-Macadam-Forge-Loulou-Montpar-Blues'];
 		//const musicbrainz = ['0c82ec60-852a-4c23-bc7b-2a4377f82702', '3921323e-d906-47fe-bc17-e5d359cc4ea3', '39049941-c348-4aeb-b86b-a5af7093d3ea'];
 		//const genius = ['Tramp-single', 'Crad-bar-boogie-single', 'Dystopie'];
-		const bandcamp = ['rocknroll-single', 'etica-nikomak', 'the-forge', 'small-pox', 'dur-d-tre-apache', 'loulou-montpar-blues-single'];
+		const bandcamp = ['rocknroll-single', 'etica-nikomak', 'the-forge', 'live', 'small-pox', 'dur-d-tre-apache', 'loulou-montpar-blues-single'];
 		const dates = {
-			fr: ['15 Novembre 2021', '7 Octobre 2021', '6 Octobre 2021', '5 Octobre 2021', '4 Octobre 2021', '17 Novembre 2021'],
-			en: ['November 15, 2021', 'October 7, 2021', 'October 6, 2021', 'October 5, 2021', 'October 4, 2021', 'November 17, 2021']
+			fr: ['15 Novembre 2021', '7 Octobre 2021', '6 Octobre 2021', '25 Novembre 2021', '5 Octobre 2021', '4 Octobre 2021', '17 Novembre 2021'],
+			en: ['November 15, 2021', 'October 7, 2021', 'October 6, 2021', 'November 25, 2021', 'October 5, 2021', 'October 4, 2021', 'November 17, 2021']
 		};
 
 		const updateUI = arrayIndex => {
